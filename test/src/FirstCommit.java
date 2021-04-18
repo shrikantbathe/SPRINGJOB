c06e4735df6369a3cc3f059a6585dc6874944032
Your Name <you@example.com>
2021-04-18 18:29:08 +0530

    added detaisl name

--- test/src/FirstCommit.java
+++ test/src/FirstCommit.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class FirstCommit {
 
 	public static void main(String[] args) {
-		String nameString = "Swati";
+		String nameString = "Swati Bathe";
 		System.out.println("Sharvil Shrikant Bathe");
 		List asdsad=null;
  
